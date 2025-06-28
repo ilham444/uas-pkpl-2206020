9e04a9a6b9cbe54218881837f06c8fd7afaeed74
ilham444 <2206020@itg.ac.id>
2025-06-28 22:47:07 +0700

    Add awal  unit tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,23 @@
 <?php
+namespace Tests\Unit;
 
-test('that true is true', function () {
-    expect(true)->toBeTrue();
-});
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+
+    // Anda bisa tambahkan test lain, misalnya untuk logika aplikasi sederhana
+    public function test_addition_function()
+    {
+        $this->assertEquals(4, 2 + 2);
+    }
+}
